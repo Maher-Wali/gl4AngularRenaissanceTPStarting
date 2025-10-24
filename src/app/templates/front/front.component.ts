489b095741555b26bf357ed81ee53e963d6a29db
Maher-Wali <walimaher00@gmail.com>
2025-10-24 22:13:49 +0100

    arc en ciel in homepage

--- src/app/templates/front/front.component.ts
+++ src/app/templates/front/front.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RainbowDirective } from '../../directives/rainbow.directive';
 
 @Component({
     selector: 'app-front',
     templateUrl: './front.component.html',
     styleUrls: ['./front.component.css'],
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, RainbowDirective]
 })
 export class FrontComponent {
 
