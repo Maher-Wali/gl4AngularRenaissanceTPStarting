4d9d1865706a866920582e76d03f6e0148391a07
erequem <ramigharbi155@gmail.com>
2025-11-30 21:36:27 +0100

    Apply OnPush change detection strategy with immutable patterns

--- src/app/optimizationPattern/rh/rh.component.ts
+++ src/app/optimizationPattern/rh/rh.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {User, UsersService} from "../users.service";
 import * as ChartJs from 'chart.js/auto';
+import { faker } from '@faker-js/faker';
+
 @Component({
   selector: 'app-rh',
   templateUrl: './rh.component.html',
@@ -10,16 +12,43 @@ export class RhComponent implements OnInit {
   oddUsers: User[];
   evenUsers: User[];
   chart: any;
+  
   constructor(private userService: UsersService) {
     this.oddUsers = this.userService.getOddOrEven(true);
     this.evenUsers = this.userService.getOddOrEven();
   }
 
   ngOnInit(): void {
-        this.createChart();
-    }
+    this.createChart();
+  }
+  
+  // IMPORTANT: Pour que OnPush fonctionne, on doit créer une NOUVELLE référence
+  // Au lieu de muter l'array existant, on crée un nouveau array
   addUser(list: User[], newUser: string) {
-    this.userService.addUser(list, newUser);
+    const newUserObj: User = {
+      name: newUser,
+      age: faker.datatype.number({min: 18, max: 30})
+    };
+    
+    // Création d'un nouveau array (immutable pattern)
+    if (list === this.oddUsers) {
+      this.oddUsers = [newUserObj, ...this.oddUsers];
+    } else {
+      this.evenUsers = [newUserObj, ...this.evenUsers];
+    }
+    
+    // Mise à jour du chart
+    this.updateChart();
+  }
+  
+  updateChart() {
+    if (this.chart) {
+      this.chart.data.datasets[0].data = [
+        this.oddUsers.length,
+        this.evenUsers.length
+      ];
+      this.chart.update();
+    }
   }
   createChart(){
     const data = [
--- src/app/optimizationPattern/user-list/user-list.component.ts
+++ src/app/optimizationPattern/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from '@angular/core';
 import {User} from "../users.service";
 
 export const fibonnaci = (n: number): number => {
@@ -11,21 +11,27 @@ export const fibonnaci = (n: number): number => {
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  // OnPush: Le composant ne se met à jour que si:
+  // 1. Une @Input change de référence
+  // 2. Un événement DOM se déclenche dans le composant
+  // 3. Un Observable/Promise émet dans le template (async pipe)
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserListComponent {
   @Input() usersCluster: string = '';
   @Input() users: User[] = [];
   @Output() add = new EventEmitter<string>();
   userFullName: string = '';
+  
   addUser() {
     this.add.emit(this.userFullName);
     this.userFullName = '';
   }
+  
   fibo(n: number): number {
     const fib = fibonnaci(n);
     console.log({n, fib});
-
     return fib;
   }
 }
